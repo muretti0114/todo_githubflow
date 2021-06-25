c85dc2cb6badf80798aa75e1ef2acc14d4bfde23
Masahide NAKAMURA <muretti0114@cs.kobe-u.ac.jp>
2021-06-25 23:51:22 +0900

    バリデーションを実装

--- todoapp_v1/src/main/java/jp/ac/kobe_u/cs/itspecialist/todoapp/controller/ToDoController.java
+++ todoapp_v1/src/main/java/jp/ac/kobe_u/cs/itspecialist/todoapp/controller/ToDoController.java
@@ -3,15 +3,17 @@ package jp.ac.kobe_u.cs.itspecialist.todoapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.Bindable.BindRestriction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.ac.kobe_u.cs.itspecialist.todoapp.dto.LoginForm;
 import jp.ac.kobe_u.cs.itspecialist.todoapp.dto.ToDoForm;
 import jp.ac.kobe_u.cs.itspecialist.todoapp.entity.Member;
 import jp.ac.kobe_u.cs.itspecialist.todoapp.entity.ToDo;
@@ -24,29 +26,38 @@ public class ToDoController {
     MemberService mService;
     @Autowired
     ToDoService tService;
+
     /**
      * トップページ
      */
     @GetMapping("/")
-    String showIndex(Model model) {
+    String showIndex(@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
+        model.addAttribute("loginForm", loginForm);
         return "index";
     }
+
     /**
      * ログイン処理．midの存在確認をして，ユーザページにリダイレクト
      */
     @PostMapping("/login")
-    String login(@RequestParam String mid, Model model) {
-        Member m = mService.getMember(mid); // 存在チェック
+    String login(@Validated @ModelAttribute(name = "loginForm") LoginForm loginForm, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) { // バリデーション
+            return showIndex(loginForm, model);
+        }
+
+        Member m = mService.getMember(loginForm.getMid()); // 存在チェック
         return "redirect:/" + m.getMid() + "/todos";
     }
+
     /**
      * ユーザのToDoリストのページ
      */
     @GetMapping("/{mid}/todos")
-    String showToDoList(@PathVariable String mid, Model model) {
+    String showToDoList(@PathVariable String mid, @ModelAttribute(name = "ToDoForm") ToDoForm form, Model model) {
         Member m = mService.getMember(mid);
         model.addAttribute("member", m);
-        ToDoForm form = new ToDoForm();
         model.addAttribute("ToDoForm", form);
         List<ToDo> todos = tService.getToDoList(mid);
         model.addAttribute("todos", todos);
@@ -54,6 +65,7 @@ public class ToDoController {
         model.addAttribute("dones", dones);
         return "list";
     }
+
     /**
      * 全員のToDoリストのページ
      */
@@ -67,15 +79,20 @@ public class ToDoController {
         model.addAttribute("dones", dones);
         return "alllist";
     }
+
     /**
      * ToDoの作成．作成処理後，ユーザページへリダイレクト
      */
     @PostMapping("/{mid}/todos")
     String createToDo(@PathVariable String mid, @Validated @ModelAttribute(name = "ToDoForm") ToDoForm form,
-            Model model) {
+            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return showToDoList(mid, form, model);
+        }
         tService.createToDo(mid, form);
         return "redirect:/" + mid + "/todos";
     }
+
     /**
      * ToDoの完了．完了処理後，ユーザページへリダイレクト
      */
--- todoapp_v1/src/main/java/jp/ac/kobe_u/cs/itspecialist/todoapp/dto/MemberForm.java
+++ todoapp_v1/src/main/java/jp/ac/kobe_u/cs/itspecialist/todoapp/dto/MemberForm.java
@@ -1,5 +1,9 @@
 package jp.ac.kobe_u.cs.itspecialist.todoapp.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import jp.ac.kobe_u.cs.itspecialist.todoapp.entity.Member;
 import lombok.Data;
 
@@ -8,8 +12,12 @@ import lombok.Data;
  */
 @Data
 public class MemberForm {
-    String mid; //メンバーID．
-    String name; //名前
+    @Pattern(regexp ="[a-z0-9_\\-]{4,16}")
+    String mid; //メンバーID．英小文字，数字，ハイフン，アンダーバー．4文字以上16文字未満．
+
+    @NotBlank
+    @Size(min = 1, max = 32)
+    String name; //名前．最大32文字
 
     public Member toEntity() {
         Member m = new Member(mid, name);
