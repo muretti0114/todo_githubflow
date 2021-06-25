2665e2a5fa25a6cd968646a89cf1f04e4daa8702
Haruaki Tamada <tamada@users.noreply.github.com>
2021-06-25 14:18:26 +0900

    alllistの方の修正が漏れていたので修正した．

---
+++ todoapp_v0/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+# データが重複していても無視する．
+INSERT IGNORE INTO member VALUES ('tamada', 'Haruaki Tamada');
+INSERT IGNORE INTO member VALUES ('masa-n', 'Masahide Nakamura');
+INSERT IGNORE INTO member VALUES ('saiki', 'Sachio Saiki');
